00cb1a5e1356223519feaaa39d5256b8fb43820f
Luana Rodrigues <85495937+lurodig@users.noreply.github.com>
2023-02-22 21:36:16 -0300

    Add files via upload

---
+++ exercicios_python/operadores_EX2.py
@@ -0,0 +1,8 @@
+#Exercício 2
+#Faça um programa que peça via linha de comando as 4 notas bimestrais e mostre a média.
+
+import sys
+
+media = sys.argv[1] + sys.argv[2] + sys.argv[3] + sys.argv[4]
+
+print(media)
